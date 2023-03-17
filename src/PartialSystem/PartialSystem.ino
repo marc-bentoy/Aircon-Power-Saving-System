dda63772efeb68742a5ff7a87a7fabeba9eeb12f
Vince <marc.bentoy@gmail.com>
2023-03-17 15:51:14 +0800

    :triangular_flag_on_post: add structure flow

--- src/PartialSystem/PartialSystem.ino
+++ src/PartialSystem/PartialSystem.ino
@@ -11,6 +11,12 @@ float temperature = 0;
 float humidity = 0;
 float heatIndex = 0;
 
+/* IR REMOTE VALUES */
+const String IR_TEMP_DOWN = "b";        // ***
+const String IR_TEMP_UP = "7";          // to be change depending on the 
+const String IR_POWER = "2";            // specific remote signal values 
+const String IR_POWER_SAVING = "68";    // *** 
+
 DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
@@ -28,12 +34,26 @@ void loop() {
     // read IRremote
     // read Temperature
     // read system settings
-    // analyze temperature and system settings
-    // perform necessary action best fit on the settings and temperature
+    // analyze actual temperature and desired tempearture 
+    // perform necessary action best fit on the desired and actual temperature
     // read voltage
     // read current
     // calculate power
     // display temperature and power
+}
+
+void readIRremote() {
+    /* TODO */
+    /* read IRremote signals */
+    /* perform specific actions based on the signal read */
+    // if ir_signal == b (volume down)
+        // decrease the desired temperature by 1 degree celsius 
+    // if ir_signal == 7 (volume up)
+        // increase the temperature by 1 degree celsius 
+    // if ir_signal == 2 (power)
+        // to be added
+    // if ir_signal == 68 (enter)
+        // toggle power saving mode after a period of time
 }
 
 void readTemperature() {
@@ -55,6 +75,7 @@ void readTemperature() {
     // Compute heat index in Celsius (isFahreheit = false)
     heatIndex = dht.computeHeatIndex(temperature, humidity, false);
 
+    // serial output for debugging
     Serial.print(F("Humidity: "));
     Serial.print(humidity);
     Serial.print(F("%  Temperature: "));
@@ -63,4 +84,24 @@ void readTemperature() {
     Serial.print(F(" Heat index: "));
     Serial.print(heatIndex);
     Serial.println(F("°C "));
+}
+
+void analyzeTemperatures() {
+    // 
+}
+
+void reachDesiredTemperature() {
+    // if not in power saving mode, desired temperature can be anything
+        // if actual temperature < desired temperature
+            // turn on relay
+        // if actual temperature > desired temperature
+            // turn off relay
+        // return
+    // if in power saving mode, desired temperature can only be between 24-27 inclusive
+        // if desired temperature is greater than MAX_POWER_SAVING_TEMP or desired temperature is lesser than MIN_POWER_SAVING_TEMP
+            // return
+        // if actual temperature < desired temperature
+            // turn on relay
+        // if actual temperature > desired temperature
+            // turn off relay
 }
